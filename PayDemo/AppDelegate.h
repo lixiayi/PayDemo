9a1eb2df85de3ba3a649393da37b8943458db4b7
lixiaoyi <lixiaoyi!9830715@163.com>
2016-09-14 16:00:49 +0800

    Initial Commit

---
+++ PayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayDemo
+//
+//  Created by xiaoyi li on 16/9/14.
+//  Copyright © 2016年 xiaoyi li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayDemo
+//
+//  Created by xiaoyi li on 16/9/14.
+//  Copyright © 2016年 xiaoyi li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
